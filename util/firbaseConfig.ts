cf1f3f9759d1adea379483666044a253e013b5a1
Shiv Prakash Singh <144921828+shivprakashsinghjii@users.noreply.github.com>
2024-04-15 23:57:01 +0530

    Update firbaseConfig.ts

--- util/firbaseConfig.ts
+++ util/firbaseConfig.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'place your FIREBASE config here',
-  authDomain: 'place your FIREBASE config here',
-  projectId: 'place your FIREBASE config here',
-  storageBucket: 'place your FIREBASE config here',
-  messagingSenderId: 'place your FIREBASE config here',
-  appId: 'place your FIREBASE config here',
+   apiKey: "AIzaSyCEvHrJ_w2vk0_I1aelqs8akkyKFbOh8Cg",
+  authDomain: "fb-clone-yt-11cc7.firebaseapp.com",
+  projectId: "fb-clone-yt-11cc7",
+  storageBucket: "fb-clone-yt-11cc7.appspot.com",
+  messagingSenderId: "141681711068",
+  appId: "1:141681711068:web:6a97001674a8eac87e2119"
 };
 
 const app = initializeApp(firebaseConfig);
